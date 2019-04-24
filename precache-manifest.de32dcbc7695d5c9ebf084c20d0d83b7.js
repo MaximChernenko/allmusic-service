7fab515c759d7559d104600c50ce76457dfd7089
Maxim Chernenko <twiggyramirez111@gmail.com>
2019-04-24 13:36:21 +0300

    Updates

---
+++ precache-manifest.de32dcbc7695d5c9ebf084c20d0d83b7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a6d204178bdfedb1dedf617a5a40b4",
+    "url": "/allmusic-service/static/media/1.22a6d204.jpg"
+  },
+  {
+    "revision": "d8c41013b30e747cd9e7",
+    "url": "/allmusic-service/static/css/main.cc444ec4.chunk.css"
+  },
+  {
+    "revision": "58e883d176dbde348995",
+    "url": "/allmusic-service/static/js/runtime~main.e79272ab.js"
+  },
+  {
+    "revision": "266bd9e69faadac1b3bb553c0afbc184",
+    "url": "/allmusic-service/static/media/search.266bd9e6.svg"
+  },
+  {
+    "revision": "c861588695bdad9f2ff4",
+    "url": "/allmusic-service/static/js/2.4656a6c7.chunk.js"
+  },
+  {
+    "revision": "9362cee637d0e0bb0d51c92b8dbf7565",
+    "url": "/allmusic-service/static/media/1.9362cee6.jpg"
+  },
+  {
+    "revision": "7dc4cf83dbd79d85577034832a39d5ed",
+    "url": "/allmusic-service/static/media/2.7dc4cf83.jpg"
+  },
+  {
+    "revision": "7431052d10a83ea8513360f223460c22",
+    "url": "/allmusic-service/static/media/3.7431052d.jpg"
+  },
+  {
+    "revision": "804f5669a86052cc29ca5e58f8ae6a95",
+    "url": "/allmusic-service/static/media/1.804f5669.jpg"
+  },
+  {
+    "revision": "9573a85a5ed5018ec2ebe011a2889972",
+    "url": "/allmusic-service/static/media/2.9573a85a.jpg"
+  },
+  {
+    "revision": "287dbe2ad89afc2260d38d4542f61926",
+    "url": "/allmusic-service/static/media/3.287dbe2a.jpg"
+  },
+  {
+    "revision": "bd8a986e828807c8f01ded6dbec52239",
+    "url": "/allmusic-service/static/media/4.bd8a986e.jpg"
+  },
+  {
+    "revision": "d9714a5558dcf3e8da2a4e01ac755b02",
+    "url": "/allmusic-service/static/media/5.d9714a55.jpg"
+  },
+  {
+    "revision": "6c85d0d2375b2e0235fd4019cdbdacd0",
+    "url": "/allmusic-service/static/media/6.6c85d0d2.jpg"
+  },
+  {
+    "revision": "d8c41013b30e747cd9e7",
+    "url": "/allmusic-service/static/js/main.bb5ec7cc.chunk.js"
+  },
+  {
+    "revision": "446150303feacd9bea8777a462a03d30",
+    "url": "/allmusic-service/static/media/2.44615030.jpg"
+  },
+  {
+    "revision": "cb7bbcd025dcd80ccda1e9283c123144",
+    "url": "/allmusic-service/static/media/3.cb7bbcd0.jpg"
+  },
+  {
+    "revision": "a281ec9114c6b0693291c61f2ac6fc58",
+    "url": "/allmusic-service/static/media/4.a281ec91.jpg"
+  },
+  {
+    "revision": "f45b4bbb4de901c41291c196905e06b6",
+    "url": "/allmusic-service/static/media/5.f45b4bbb.jpg"
+  },
+  {
+    "revision": "63d8f7477d417f5c9818d37ac2eab509",
+    "url": "/allmusic-service/static/media/6.63d8f747.jpg"
+  },
+  {
+    "revision": "592a501ecc7329bcfe1e4d0324a15dfc",
+    "url": "/allmusic-service/static/media/7.592a501e.jpg"
+  },
+  {
+    "revision": "7db15982b903ff11d19ab40adc8f76a8",
+    "url": "/allmusic-service/static/media/8.7db15982.jpg"
+  },
+  {
+    "revision": "7b8ab208874de96101766c30457f98c3",
+    "url": "/allmusic-service/static/media/9.7b8ab208.jpg"
+  },
+  {
+    "revision": "eb4bbfb234811cd30237c7c90ffee3ef",
+    "url": "/allmusic-service/static/media/10.eb4bbfb2.jpg"
+  },
+  {
+    "revision": "83994b64216f1973d767f286bf8a5c9c",
+    "url": "/allmusic-service/static/media/11.83994b64.jpg"
+  },
+  {
+    "revision": "fd140f0fc8304cdf4443abc6b4a8e1e5",
+    "url": "/allmusic-service/static/media/12.fd140f0f.jpg"
+  },
+  {
+    "revision": "0619531cb8f0eaf99c771d183f64cadd",
+    "url": "/allmusic-service/static/media/close.0619531c.svg"
+  },
+  {
+    "revision": "c861588695bdad9f2ff4",
+    "url": "/allmusic-service/static/css/2.45aa772e.chunk.css"
+  },
+  {
+    "revision": "54bdde1c75a36d773b6340652b2480e2",
+    "url": "/allmusic-service/index.html"
+  }
+];
