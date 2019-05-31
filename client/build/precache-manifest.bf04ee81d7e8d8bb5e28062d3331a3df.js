ece4ecc02281f1a036398f1e63d5cfafd7647e0b
Maxim Chernenko <twiggyramirez111@gmail.com>
2019-05-31 23:25:57 +0300

    heroku added to client

---
+++ client/build/precache-manifest.bf04ee81d7e8d8bb5e28062d3331a3df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "525f883a39049b5af26d97fbd290ef2b",
+    "url": "/allmusic-service/static/media/star-blue.525f883a.svg"
+  },
+  {
+    "revision": "a952b8af8f1ecb443fbf",
+    "url": "/allmusic-service/static/css/main.0c75fd75.chunk.css"
+  },
+  {
+    "revision": "58e883d176dbde348995",
+    "url": "/allmusic-service/static/js/runtime~main.e79272ab.js"
+  },
+  {
+    "revision": "6072894c8eaf025b21c18af9afec4bc9",
+    "url": "/allmusic-service/static/media/icons.6072894c.svg"
+  },
+  {
+    "revision": "4ff559c85732fe114b9c",
+    "url": "/allmusic-service/static/js/2.a3d618eb.chunk.js"
+  },
+  {
+    "revision": "f3181f3538e881d641fa4f326798b323",
+    "url": "/allmusic-service/static/media/remove.f3181f35.svg"
+  },
+  {
+    "revision": "7f55f804d4df9b170bf5849498448e2c",
+    "url": "/allmusic-service/static/media/star.7f55f804.svg"
+  },
+  {
+    "revision": "a952b8af8f1ecb443fbf",
+    "url": "/allmusic-service/static/js/main.22c71717.chunk.js"
+  },
+  {
+    "revision": "8d84e133f7616e003a0f16b531c2780d",
+    "url": "/allmusic-service/static/media/star-red.8d84e133.svg"
+  },
+  {
+    "revision": "affdf25fb7a9ba26816e005d7ebd3146",
+    "url": "/allmusic-service/static/media/star-orange.affdf25f.svg"
+  },
+  {
+    "revision": "b09df884c74dd7e30f2f37ff78b3d18b",
+    "url": "/allmusic-service/static/media/sound.b09df884.svg"
+  },
+  {
+    "revision": "266bd9e69faadac1b3bb553c0afbc184",
+    "url": "/allmusic-service/static/media/search.266bd9e6.svg"
+  },
+  {
+    "revision": "4ff559c85732fe114b9c",
+    "url": "/allmusic-service/static/css/2.8c71954c.chunk.css"
+  },
+  {
+    "revision": "68923e659bc9abac06ad82ddf05bef24",
+    "url": "/allmusic-service/index.html"
+  }
+];
